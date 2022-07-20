38e77b4748d7a60a17b1f72e1b59ed4d6870ce54
aytekin_desktop <mjeong827@gmail.com>
2022-07-20 19:15:01 +0900

    UserCreateView try-except validation

--- apis/views.py
+++ apis/views.py
@@ -25,9 +25,18 @@ class UserCreateView(BaseView):
 
     def post(self, request):
         username = request.POST.get('username','')
+        if not username:
+            return self.response(message='아이디를 입력해주세요', status=400)
         password = request.POST.get('password','')
+        if not password:
+            return self.response(message='패스워드를 입력해주세요.', status=400)
         email = request.POST.get('email','')
+        if not email:
+            return self.response(message='이메일를 입력해주세요.', status=400)
 
-        user = User.object.create_user(username, password, email)
+        try:
+            user = User.object.create_user(username, password, email)
+        except IntegrityError:
+            return self.response(message='이미 존재하는 아이디입니다.', status=400)
 
         return self.response({'user.id':user.id})
