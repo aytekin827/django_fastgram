c88ad9553bf5950c2aaf964ec862a8273a5a2c7e
aytekin_desktop <mjeong827@gmail.com>
2022-07-22 21:01:39 +0900

    contents 앱 admin설정(inlines속성 설정)

--- contents/admin.py
+++ contents/admin.py
@@ -1,15 +1,18 @@
 from django.contrib import admin
 from .models import Image, Content
 
+
+class ImageInline(admin.TabularInline):
+    model = Image
+
+
 class ImageAdmin(admin.ModelAdmin):
     list_display = ('order','content')
-
-
 admin.site.register(Image, ImageAdmin)
 
 
 class ContentAdmin(admin.ModelAdmin):
-    list_display = ('user','text')
-
+    list_display = ('user','text','created_at')
+    inlines = [ImageInline] # 동일한 페이지에서 image모델과 content모델 둘다 볼 수 있다.
 
 admin.site.register(Content, ContentAdmin)
