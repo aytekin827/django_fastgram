41f2e9389668cb8ef4b2e007eb3f311e1d7939ce
aytekin_desktop <mjeong827@gmail.com>
2022-07-22 20:54:16 +0900

    contents 앱 이미지 모델 정의

--- contents/admin.py
+++ contents/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Image, Content
 
-# Register your models here.
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ('order','content')
+
+
+admin.site.register(Image, ImageAdmin)
+
+
+class ContentAdmin(admin.ModelAdmin):
+    list_display = ('user','text')
+
+
+admin.site.register(Content, ContentAdmin)
--- contents/models.py
+++ contents/models.py
@@ -1,3 +1,37 @@
-from django.db import models
+import os
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Content(BaseModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField(default='')
+
+    def __str__(self):
+        return  self.text + 'created by_' + self.user.username
+
+def image_upload_to(instance, filename):
+    ext = filename.split('.')[-1]
+    return os.path.join(instance.UPLOAD_PATH, "%s.%s" % (uuid.uuid4(), ext))
+
+
+class Image(BaseModel):
+    UPLOAD_PATH = 'user-upload'
+
+    content = models.ForeignKey(Content, on_delete=models.CASCADE)
+    iamge = models.ImageField(upload_to=image_upload_to)
+    order = models.SmallIntegerField() #image numbering
+
+    class Meta:
+        ordering = ['order']
+
